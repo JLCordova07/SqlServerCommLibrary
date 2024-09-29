26415bae7accdeac0335fb7ddb5d23b091b0340e
Jose Luis Cordova <134427828+JLCordova07@users.noreply.github.com>
2024-09-29 12:26:37 -0500

    Add files via upload

---
+++ DataHandler.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Lambda_Release01._01._01.BackEnd.DataAccessLayer.DataHandlers
+{
+    public static class DataHandler
+    {
+        public static ObservableCollection<T> FetchData<T>(string storedProcedureName, DynamicParameters parameters = null)
+        {
+            return new ObservableCollection<T>(SqlServerComm.ExecuteStoredProcedure<T>(storedProcedureName, parameters));
+        }
+        public static int SaveData(string storedProcedureName, DynamicParameters parameters)
+        {
+            return SqlServerComm.ExecuteCommand(storedProcedureName, parameters);
+        }
+    }
+}
---
+++ SqlServerComm.cs
@@ -0,0 +1,97 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Lambda_Release01._01._01.BackEnd.DataAccessLayer
+{
+    public static class SqlServerComm
+    {
+        internal static SqlConnection _connection = new SqlConnection();
+        internal static string _connectionString;
+        internal static string _commandString;
+        public static void OpenConnection()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    _connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error while trying to open connection. \n" + ex.Message);
+                }
+            }
+        }
+        public static void CloseConnection()
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                try
+                {
+                    _connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error while trying to close connection. \n" + ex.Message);
+                }
+            }
+        }
+        private static void PrepareConnection(string connectionStringName, string storedProcedureName)
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+            _connection.ConnectionString = _connectionString;
+            _commandString = "Exec " + storedProcedureName;
+        }
+        private static void PrepareParameters(DynamicParameters dynamicParameters)
+        {
+            if (dynamicParameters.ParameterNames.Count() > 0)
+            {
+                foreach (string parameterName in dynamicParameters.ParameterNames)
+                {
+                    if (parameterName == dynamicParameters.ParameterNames.Last<string>())
+                    {
+                        _commandString += " @" + parameterName;
+                    }
+                    else
+                    {
+                        _commandString += " @" + parameterName + ", ";
+                    }
+                }
+            }
+        }
+        public static int ExecuteCommand(string storedProcedureName, DynamicParameters dynamicParameters = null, string connectionStringName = "default")
+        {
+            PrepareConnection(connectionStringName, storedProcedureName);
+            dynamicParameters = dynamicParameters ?? new DynamicParameters();
+            if (dynamicParameters.ParameterNames.Count() > 0)
+            {
+                PrepareParameters(dynamicParameters);
+                return _connection.Execute(_commandString, dynamicParameters);
+            }
+            else
+            {
+                return _connection.Execute(_commandString);
+            }
+        }
+        public static IEnumerable<T> ExecuteStoredProcedure<T>(string storedProcedureName, DynamicParameters dynamicParameters = null, string connectionStringName = "default")
+        {
+            PrepareConnection(connectionStringName, storedProcedureName);
+            dynamicParameters = dynamicParameters ?? new DynamicParameters();
+            if (dynamicParameters.ParameterNames.Count() > 0)
+            {
+                PrepareParameters(dynamicParameters);
+                return _connection.Query<T>(_commandString, dynamicParameters);
+            }
+            else
+            {
+                return _connection.Query<T>(_commandString);
+            }
+        }
+    }
+}
